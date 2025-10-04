1c0f03f381850e7931e07ba5c05921902c2c0462
Haneen Ibrahim <haneenibrahim750@gmail.com>
2025-10-04 17:01:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector("#red").onclick = function() {
+    document.querySelector("#demo").style.color = "red";
+}
+
+document.querySelector("#green").onclick = function() {
+    document.querySelector("#demo").style.color = "green";
+}
+
+document.querySelector("#yellow").onclick = function() {
+    document.querySelector("#demo").style.color = "yellow";
+}
